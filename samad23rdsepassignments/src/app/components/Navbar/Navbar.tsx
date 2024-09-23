c81754dbc38ad4fb678be0933c49accb578deaab
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-09-23 23:35:44 +0500

    1135pm

--- samad23rdsepassignments/src/app/components/Navbar/Navbar.tsx
+++ samad23rdsepassignments/src/app/components/Navbar/Navbar.tsx
@@ -5,8 +5,8 @@ const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/contact">Contact</Link>
+      <Link href="/About">About</Link>
+      <Link href="/Contact">Contact</Link>
     </nav>
   );
 };
