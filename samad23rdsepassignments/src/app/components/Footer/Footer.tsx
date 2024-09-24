6e9f0ce6b2daecaf2486ed3b35675bae33a7a643
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-09-24 14:47:26 +0500

    247 24 09 2024

---
+++ samad23rdsepassignments/src/app/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+    return(
+    <footer className={styles.footer}>
+        <p>SAMADDEVELOPER @ linkedin </p>
+
+    </footer>
+
+    )
+}
+
+export default Footer
--- samad23rdsepassignments/src/app/components/ParentComponent/ParentComponent.tsx
+++ samad23rdsepassignments/src/app/components/ParentComponent/ParentComponent.tsx
@@ -1,12 +1,13 @@
-
-import ChildComponent from '../ChildComponent/ChildComponent';
+import React from 'react';
+import ChildComponent from './ChildComponent';
 
 const ParentComponent = () => {
-  const message = "Hello from Parent!";
+  const parentData = "Hello from Parent!";
+
   return (
     <div>
-      <h1>Parent Component</h1>
-      <ChildComponent message={message} />
+      <h2>Parent Component</h2>
+      <ChildComponent message={parentData} />
     </div>
   );
 };
