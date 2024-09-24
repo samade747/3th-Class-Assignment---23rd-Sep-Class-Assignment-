ec97e1f4d7f5d693cc4d4498e7a813e5806db4c8
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-09-24 19:24:18 +0500

    hggh

--- samadmilestonenextjs1/next.config.mjs
+++
@@ -1,8 +0,0 @@
-// next.config.mjs
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  pageExtensions: ['tsx', 'ts', 'jsx', 'js'], // Ensure TypeScript and JavaScript files are recognized
-};
-
-export default nextConfig;
