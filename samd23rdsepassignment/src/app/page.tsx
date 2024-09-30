8c83d4de09831d2a535813ac07125cd033d302f0
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-10-01 00:15:07 +0500

    Update page.tsx

--- samd23rdsepassignment/src/app/page.tsx
+++ samd23rdsepassignment/src/app/page.tsx
@@ -3,27 +3,6 @@ import ChildComponent from './child/ChildComponent';
 import { useRouter } from 'next/navigation'; 
 
 
-// export default function Home() {
-//   return (
-//     <main>
-//       <h1>Welcome to the Home Page</h1>
-
-      
-//       <ChildComponent 
-//         title="This is the Child Component"
-//         title1="Props Example"
-//         description="Props are passed from the parent Home component."
-//       />
-
-//       <ChildComponent 
-//         title="Another Child Component"
-//         title1="Props Example 2"
-//         description="This is a second instance of the child component with different props."
-//       />
-//     </main>
-//   );
-// }
-
 
 
 
